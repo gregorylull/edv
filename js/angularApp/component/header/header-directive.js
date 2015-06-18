b9cb679ef8c46543827659dcc9f3ce8a27edc7b1
gregorylull <gregorylull@gmail.com>
2015-06-18 01:23:05 -0700

    pre foundation and libsass integration

--- js/angularApp/component/header/header-directive.js
+++ js/angularApp/component/header/header-directive.js
@@ -1,9 +1,11 @@
-angular.module('edv-header-directive', [])
-.directive('edvHeader', function () {
-  console.log('inside directive function');
-  return {
-    restrict: "A",
-    templateUrl: edvThemeLocal.ngPath + "/component/header/header-directive.html",
-  };
-});
- 
+(function (test) {
+  console.log(test);
+  angular.module('edv-header-directive', [])
+  .directive('edvHeader', function () {
+    console.log('inside directive function');
+    return {
+      restrict: "A",
+      templateUrl: edvThemeLocal.ngPath + "/component/header/header-directive.html",
+    };
+  });
+})();
