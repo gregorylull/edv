6e604cfc18bd58c277dd61918792fdd460675fd5
gregorylull <gregorylull@gmail.com>
2015-06-29 16:04:36 -0700

    testing ngroute

--- js/angularApp/component/navbar/navbar-directive.js
+++ js/angularApp/component/navbar/navbar-directive.js
@@ -1,2 +1,7 @@
 angular.module('edv-navbar', [])
-.directive('edvNavbar', function () {});
+.directive('edvNavbar', function () {
+  return {
+    restrict: "A",
+    templateUrl: edvContent.ngPath + "/component/navbar/navbar-directive.html"
+  };
+});
