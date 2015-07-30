6768516f13de74dcddc5292f1b4b37b50c56ff93
gregorylull <gregorylull@gmail.com>
2015-07-30 15:16:13 -0400

    starting with header first

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-'use strict';
+// 
 (function (window) {
+'use strict';
 
   angular.module("edvApp", [
     'ngRoute',
@@ -10,15 +11,6 @@
     var componentPath = edvContent.ngPath + "component/";
     console.log('route provider executed', componentPath);
     $routeProvider
-      .when('/test1', {
-        templateUrl: componentPath + 'test1.html'
-      })
-      .when('/test2', {
-        templateUrl: componentPath + 'test2.html'
-      })
-      .when('/test3', {
-        templateUrl: componentPath + 'test3.html'
-      })
       .otherwise({
         redirctTo: '/'
       });
--- js/angularApp/component/header/header-directive.js
+++ js/angularApp/component/header/header-directive.js
@@ -1,12 +1,13 @@
-(function () {
-///
+// 
+(function (window) {
+'use strict';
 angular.module('edv-header', [])
 .directive('edvHeader', function () {
-  console.log('inside directive function');
+  console.log('edv-header: first line');
   return {
     restrict: "A",
     templateUrl: edvContent.ngPath + "/component/header/header-directive.html",
   };
 });
 
-})();
+})(window);
