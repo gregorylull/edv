bd4ab932dc3164ce30998f50981714e100d9a82c
gregorylull <gregorylull@gmail.com>
2015-07-30 16:13:59 -0400

    Added a edventure logo directive which can be placed into anywhere on the page as an attribute 'edv-logo'. Can take arguments, which will retrieve the appropriate image, three so far: 'all' -- combination of edventure picture and words 'picture' -- just pictures 'words' -- just words

--- js/angularApp/component/header/header-directive.js
+++ js/angularApp/component/header/header-directive.js
@@ -1,12 +1,12 @@
 // 
 (function (window) {
 'use strict';
-angular.module('edv-header', [])
+angular.module('edv-header', ['edv-logo'])
 .directive('edvHeader', function () {
   console.log('edv-header: first line');
   return {
     restrict: "A",
-    templateUrl: edvContent.ngPath + "/component/header/header-directive.html",
+    templateUrl: edvContent.ngPath + "/component/header/header-directive.html"
   };
 });
 
---
+++ js/angularApp/component/logo/logo-directive.js
@@ -0,0 +1,38 @@
+// to retrieve the logo, either the whole logo, or the words only or image
+(function (window) {
+  'use strict';
+  angular.module('edv-logo', [])
+  .directive('edvLogo', [function () {
+    return {
+      restrict: 'A',
+      scope: {
+        'logoType' : '@edvLogo'
+      },
+      controller: function ($scope) {
+        var logo = {};
+        logo.style = $scope.logoType;
+
+        switch ($scope.logoType) {
+          case 'all':
+            logo.alt = "EdVentures Logo, picture and words";
+            logo.source = "http://localhost:1337/wp-content/uploads/2015/07/logoWithWords.png";
+            break;
+          
+          case 'words':
+            logo.alt = "EdVentures Logo, words only";
+            break;
+
+          case 'picture':
+            logo.alt = "EdVentures Logo, picture only";
+          default:
+            logo.alt = "EdVentures Logo";
+            break;
+        }
+
+        $scope.logo = logo;
+      },
+      templateUrl: edvContent.ngPath + "/component/logo/logo-directive.html",
+      replace: true
+    }
+  }]);
+})(window);
